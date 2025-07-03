db27465d0ccbbd41796380df3541ddbaa520d6c1
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-07-03 17:18:35 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 57868 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,19 +1,21 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main{
+    
+    public static int[] arr;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int N = Integer.parseInt(br.readLine());
         
-        int[] arr = new int[N];
+        arr = new int[N];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -27,12 +29,30 @@ public class Main{
         StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < M; i++){
-            if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0){
+            if(binarySearch(Integer.parseInt(st.nextToken())) >= 0){
                 sb.append(1).append("\n");
             } else {
                 sb.append(0).append("\n");
             }
         }
         System.out.println(sb);
+    }
+    
+    public static int binarySearch(int key){
+        int lo = 0;
+        int hi = arr.length - 1;
+        
+        while(lo <= hi){
+            int mid = (lo + hi) / 2;
+            
+            if(key < arr[mid]){
+                hi = mid - 1;
+            } else if(key > arr[mid]){
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
