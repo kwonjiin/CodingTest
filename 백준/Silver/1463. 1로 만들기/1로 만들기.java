992572f100a915145fef2147a421a4b2e045aaf6
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-08-14 14:27:46 +0900

    [Silver III] Title: 1로 만들기, Time: 72 ms, Memory: 12608 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main{
+public class Main {
+    static class Node {
+        int num;
+        int cnt;
+        Node(int num, int cnt) {
+            this.num = num;
+            this.cnt = cnt;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
-        
-        int[] dp = new int[X + 1];
-        
-        for(int i = 2; i <= X; i++){
-            dp[i] = dp[i -1] + 1;
-            
-            if(i % 2 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(X, 0));
+
+        boolean[] visited = new boolean[X + 1];
+        visited[X] = true;
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.num == 1) {
+                System.out.println(cur.cnt);
+                return;
             }
-            if(i % 3 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+
+            // 3으로 나누기
+            if (cur.num % 3 == 0 && !visited[cur.num / 3]) {
+                visited[cur.num / 3] = true;
+                q.add(new Node(cur.num / 3, cur.cnt + 1));
+            }
+
+            // 2로 나누기
+            if (cur.num % 2 == 0 && !visited[cur.num / 2]) {
+                visited[cur.num / 2] = true;
+                q.add(new Node(cur.num / 2, cur.cnt + 1));
+            }
+
+            // 1 빼기
+            if (!visited[cur.num - 1]) {
+                visited[cur.num - 1] = true;
+                q.add(new Node(cur.num - 1, cur.cnt + 1));
             }
         }
-        System.out.println(dp[X]);
     }
 }
