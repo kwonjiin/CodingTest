9523169f3202050c4baea282faef68a9c168d947
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-08-14 15:44:25 +0900

    [Bronze III] Title: 공 넣기, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -9,22 +9,22 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] basket = new int[N + 1];
+        int[] bucket = new int[N + 1];
 
         for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
 
             // Arrays.fill을 사용해 i~j까지 한 번에 채움
-            Arrays.fill(basket, i, j + 1, k);
+            Arrays.fill(bucket, start, end + 1, num);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int idx = 1; idx <= N; idx++) {
-            sb.append(basket[idx]).append(" ");
+            sb.append(bucket[idx]).append(" ");
         }
-        System.out.print(sb.toString().trim());
+        System.out.print(sb);
     }
 }
