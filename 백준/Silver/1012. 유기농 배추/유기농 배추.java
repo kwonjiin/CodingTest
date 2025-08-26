b3886660d6dd0d83c14199f93a2d30f61b61b64e
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-08-26 22:03:35 +0900

    [Silver II] Title: 유기농 배추, Time: 108 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int M, N, K;
+    static int[][] field;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            field = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                field[y][x] = 1;
+            }
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (field[i][j] == 1 && !visited[i][j]) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    static void bfs(int y, int x) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{y, x});
+        visited[y][x] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int d = 0; d < 4; d++) {
+                int ny = cur[0] + dy[d];
+                int nx = cur[1] + dx[d];
+                if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+                    if (field[ny][nx] == 1 && !visited[ny][nx]) {
+                        visited[ny][nx] = true;
+                        q.add(new int[]{ny, nx});
+                    }
+                }
+            }
+        }
+    }
+}
