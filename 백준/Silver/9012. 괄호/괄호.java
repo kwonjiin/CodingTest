e67397d7f8ca028e3ead7294d403d6e31081dbaf
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-08-27 23:59:52 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,29 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
+            int count = 0;
             boolean isValid = true;
 
             for (char ch : str.toCharArray()) {
-                if (ch == '(') {
-                    stack.push(ch); // 열린 괄호 추가
-                } else { // ')'
-                    if (stack.isEmpty()) {
-                        isValid = false; // 닫을 괄호가 없음
-                        break;
-                    }
-                    stack.pop(); // 짝 맞춰 제거
+                if (ch == '(') count++;
+                else count--;
+
+                if (count < 0) { // 닫는 괄호가 먼저 나오면 실패
+                    isValid = false;
+                    break;
                 }
             }
 
-            if (!stack.isEmpty()) isValid = false; // 다 닫히지 않음
+            if (count != 0) isValid = false;
 
             System.out.println(isValid ? "YES" : "NO");
         }
