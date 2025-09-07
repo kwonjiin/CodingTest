d5cc30757ffe6fdb5cf6a6aa699604f02dae2d21
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-07 22:11:27 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 152 ms, Memory: 17208 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            String line = br.readLine();
+            if (line.equals(".")) break; // 종료 조건
+            
+            Stack<Character> stack = new Stack<>();
+            boolean isBalanced = true;
+            
+            for (char ch : line.toCharArray()) {
+                if (ch == '(' || ch == '[') {
+                    stack.push(ch);
+                } else if (ch == ')') {
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        isBalanced = false;
+                        break;
+                    }
+                    stack.pop();
+                } else if (ch == ']') {
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        isBalanced = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            
+            if (!stack.isEmpty()) isBalanced = false;
+            sb.append(isBalanced ? "yes\n" : "no\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
