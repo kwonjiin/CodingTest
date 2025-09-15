aa2be56ceb8e793e9860fd14251ae5bf9c13559f
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-15 21:31:43 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 16828 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
-        }
+        for (int i = 0; i < N; i++) A[i] = sc.nextInt();
 
-        int[] dp = new int[N];
-        int max = 0;
+        ArrayList<Integer> lis = new ArrayList<>();
+        lis.add(A[0]);
 
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1; // 최소 길이 1
-            for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        for (int i = 1; i < N; i++) {
+            if (A[i] > lis.get(lis.size() - 1)) {
+                lis.add(A[i]);
+            } else {
+                // 이분 탐색으로 교체 위치 찾기
+                int left = 0, right = lis.size() - 1;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (lis.get(mid) >= A[i]) right = mid;
+                    else left = mid + 1;
                 }
+                lis.set(right, A[i]);
             }
-            max = Math.max(max, dp[i]);
         }
 
-        System.out.println(max);
+        System.out.println(lis.size());
     }
 }
