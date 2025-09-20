6581881e1f36532ee715e96066eee9f2cfa8be33
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-20 23:01:38 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,32 +1,31 @@
 import java.io.*;
 
 public class Main {
+    static int[] stair, dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] stair = new int[N + 1];
+        stair = new int[N + 1];
+        dp = new int[N + 1];
+
         for (int i = 1; i <= N; i++) {
             stair[i] = Integer.parseInt(br.readLine());
+            dp[i] = -1;
         }
 
-        if (N == 1) {
-            System.out.println(stair[1]);
-            return;
-        } else if (N == 2) {
-            System.out.println(stair[1] + stair[2]);
-            return;
-        }
+        System.out.println(solve(N));
+    }
 
-        int[] dp = new int[N + 1];
-        dp[1] = stair[1];
-        dp[2] = stair[1] + stair[2];
-        dp[3] = Math.max(stair[1] + stair[3], stair[2] + stair[3]);
+    static int solve(int n) {
+        if (n == 1) return stair[1];
+        if (n == 2) return stair[1] + stair[2];
+        if (n == 3) return Math.max(stair[1] + stair[3], stair[2] + stair[3]);
 
-        for (int i = 4; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
-        }
+        if (dp[n] != -1) return dp[n];
 
-        System.out.println(dp[N]);
+        dp[n] = Math.max(solve(n - 2) + stair[n], solve(n - 3) + stair[n - 1] + stair[n]);
+        return dp[n];
     }
 }
