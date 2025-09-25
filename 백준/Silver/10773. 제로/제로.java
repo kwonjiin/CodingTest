333c1cb56b18c30bcba66c9660bb24069140e314
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-25 13:34:49 +0900

    [Silver IV] Title: 제로, Time: 564 ms, Memory: 94064 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -3,21 +3,20 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int K = sc.nextInt();
-        int[] stack = new int[K];
-        int top = -1;
+        Deque<Integer> stack = new ArrayDeque<>();
         
         for(int i=0;i<K;i++){
             int num = sc.nextInt();
             if(num == 0){
-                top--; // 최근 값 제거
+                stack.pollLast(); // 최근 값 제거
             } else {
-                stack[++top] = num;
+                stack.addLast(num);
             }
         }
         
-        long sum = 0; // 값 합이 클 수 있으므로 long
-        for(int i=0;i<=top;i++){
-            sum += stack[i];
+        long sum = 0;
+        for(int val : stack){
+            sum += val;
         }
         System.out.println(sum);
     }
