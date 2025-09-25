a24fa0ae45f2764b1ee55b7327e2e421c85289f1
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-25 13:33:39 +0900

    [Silver IV] Title: 제로, Time: 616 ms, Memory: 93808 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0;i<K;i++){
+            int num = sc.nextInt();
+            if(num == 0){
+                stack.pop(); // 가장 최근 값 제거
+            } else {
+                stack.push(num);
+            }
+        }
+        
+        int sum = 0;
+        for(int val : stack){
+            sum += val;
+        }
+        System.out.println(sum);
+    }
+}
