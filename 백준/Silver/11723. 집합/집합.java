20171f1a6f078056a670e3773a7af5fdfe201e6d
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-28 19:27:36 +0900

    [Silver V] Title: 집합, Time: 1160 ms, Memory: 326024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,32 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
+
+        boolean[] arr = new boolean[21];
 
         for(int i=0; i<M; i++){
             String[] cmd = br.readLine().split(" ");
             String op = cmd[0];
 
             if(op.equals("add")){
-                set.add(Integer.parseInt(cmd[1]));
+                arr[Integer.parseInt(cmd[1])] = true;
             } else if(op.equals("remove")){
-                set.remove(Integer.parseInt(cmd[1]));
+                arr[Integer.parseInt(cmd[1])] = false;
             } else if(op.equals("check")){
-                sb.append(set.contains(Integer.parseInt(cmd[1])) ? 1 : 0).append("\n");
+                sb.append(arr[Integer.parseInt(cmd[1])] ? 1 : 0).append("\n");
             } else if(op.equals("toggle")){
                 int x = Integer.parseInt(cmd[1]);
-                if(set.contains(x)) set.remove(x);
-                else set.add(x);
+                arr[x] = !arr[x];
             } else if(op.equals("all")){
-                set.clear();
-                for(int j=1; j<=20; j++) set.add(j);
+                for(int j=1; j<=20; j++) arr[j] = true;
             } else if(op.equals("empty")){
-                set.clear();
+                arr = new boolean[21];
             }
         }
         System.out.print(sb);
