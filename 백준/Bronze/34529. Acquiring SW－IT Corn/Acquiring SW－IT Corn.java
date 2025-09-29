336e5959b79027efbd9e264c836df7505f240a94
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-09-29 23:45:32 +0900

    [Bronze IV] Title: Acquiring SW-IT Corn, Time: 96 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.java
+++ 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.java
@@ -3,21 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt(), Y = sc.nextInt(), Z = sc.nextInt();
+        int U = sc.nextInt(), V = sc.nextInt(), W = sc.nextInt();
 
-        int X = sc.nextInt(); // A사 100g 가격
-        int Y = sc.nextInt(); // B사 50g 가격
-        int Z = sc.nextInt(); // C사 20g 가격
-
-        int U = sc.nextInt(); // A사 구매량 (g)
-        int V = sc.nextInt(); // B사 구매량 (g)
-        int W = sc.nextInt(); // C사 구매량 (g)
-
-        int costA = (U / 100) * X;
-        int costB = (V / 50) * Y;
-        int costC = (W / 20) * Z;
-
-        int total = costA + costB + costC;
-
+        int total = (U / 100) * X + (V / 50) * Y + (W / 20) * Z;
         System.out.println(total);
     }
 }
