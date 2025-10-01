7a1980b37793953a8638286cb9c476f40259f1ec
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-10-01 15:22:53 +0900

    [Bronze IV] Title: 2, 4, 6, 8, Time: 64 ms, Memory: 11184 KB -BaekjoonHub

--- 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.java
+++ 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         for (int n = 10; n < 100; n++) {
-            String s = Integer.toString(n);
-            String rev = new StringBuilder(s).reverse().toString();
-            int revNum = Integer.parseInt(rev);
+            int a = n / 10, b = n % 10; // a: 십의 자리, b: 일의 자리
+            int rev = b * 10 + a;
+            int sum = a + b;
 
-            int sum = (n / 10) + (n % 10);
-
-            if (revNum % 4 == 0 && sum % 6 == 0 && !s.contains("8")) {
+            if (rev % 4 == 0 && sum % 6 == 0 && a != 8 && b != 8) {
                 System.out.println(n);
                 return;
             }
