7365569a5b55f73408f492a3a14816cfe779f0a2
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-10-08 23:02:13 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 72 ms, Memory: 12332 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,22 +1,28 @@
 import java.io.*;
 
 public class Main {
+    // 문자열 s를 count번 반복해서 반환 (Java 8 호환)
+    private static String repeat(String s, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) sb.append(s);
+        return sb.toString();
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        // 윗부분
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N - i; j++) System.out.print(" ");
-            for (int j = 1; j <= 2 * i - 1; j++) System.out.print("*");
-            System.out.println();
+            sb.append(repeat(" ", N - i));
+            sb.append(repeat("*", 2 * i - 1)).append("\n");
         }
 
-        // 아랫부분
         for (int i = N - 1; i >= 1; i--) {
-            for (int j = 1; j <= N - i; j++) System.out.print(" ");
-            for (int j = 1; j <= 2 * i - 1; j++) System.out.print("*");
-            System.out.println();
+            sb.append(repeat(" ", N - i));
+            sb.append(repeat("*", 2 * i - 1)).append("\n");
         }
+
+        System.out.print(sb.toString());
     }
 }
