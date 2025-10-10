d4a6cec43b2be84121a4315bfdc656b0d5f85c0d
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-10-10 22:43:13 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 92 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
 public class Main {
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
 
-        int a = A, b = B;
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
+        int g = gcd(A, B);
+        int l = (A * B) / g;
 
-        int gcd = a;
-        int lcm = (A * B) / gcd;
-
-        System.out.println(gcd);
-        System.out.println(lcm);
+        System.out.println(g);
+        System.out.println(l);
     }
 }
