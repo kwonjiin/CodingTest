a3957d5c80a9ecab0101e941f6696433ebcd786f
권지민 <109593487+kwonjiin@users.noreply.github.com>
2025-12-03 15:18:17 +0900

    [Bronze V] Title: 이중 반복문, Time: 64 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n > 10000) {
+            System.out.println("Time limit exceeded");
+        } else {
+            System.out.println("Accepted");
+        }
+    }
+}
